ca2ce6787bcbe6f56ee5a7df5da587472b273b95
udoye <udoyeekene9@gmail.com>
2023-12-08 19:31:42 +0100

    state

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+ constructor (props){
+  super(props)
+  this.state = {
+  Person: {
+    fullName: "Lionel Messi",
+    bio: "Greatest of all Time",
+    imgSrc: "https://wallpapers.com/images/hd/messi-pictures-jzykf84saw6wbkd6.jpg" ,
+    profession: "Footballer",
+  },
+  show: false,
+};
+this.showprofile = this.showprofile.bind(this)
+} 
+  
+
+  showprofile (){
+    this.setState(prevstate=> ({
+      show: !prevstate.show
+    }))
+  }
+    
+
+  
+  render() {
+    const { fullName, bio, imgSrc, profession } = this.state.Person;
+
+    return (
+      <div className="App">
+        {this.state.show&&<><h1>{fullName}</h1>
+        <p>{bio}</p>
+        <img src={imgSrc} alt={fullName} width="300px" height="200px" />
+        <p>{profession}</p></>}
+        <button onClick={this.showprofile}>{this.state.show ? "Hide Profile" : "Show Profile"}</button>
+        
+      </div>
+    );
+  }
 }
 
 export default App;
+
